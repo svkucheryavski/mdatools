1987196ade40651cb68357f62380df1d3965bb0d
Sergey Kucheryavskiy <sergeykucheryavskiy@Sergeys-Mini.lan>
2019-12-03 19:53:21 +0100

    remove unnecessary code

--- test_plots.R
+++ test_plots.R
@@ -8,17 +8,3 @@ devtools::test_file("tests/testthat/test-mdaplot2.R")
 devtools::test_file("tests/testthat/test-mdaplotg.R")
 dev.off()
 options(device = "quartz")
-
-
-data(simdata)
-x = simdata$spectra.c
-y = simdata$conc.c[, 1]
-
-model = pls(x, y, ncomp = 8, cv = 1)
-summary(model)
-plot.new()
-plot(model)
-
-plotRegcoeffs(model)
-
-plot(model$coeffs)
