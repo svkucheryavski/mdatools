e2f5a8f0d48114318182752e435813fe1c192a22
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2020-06-05 20:47:11 +0200

    small changes in current initial tests

--- tests/testthat/test-mcrals.R
+++ tests/testthat/test-mcrals.R
@@ -14,11 +14,47 @@
 #})
 #
 
-data(simdata)
-D <- simdata$spectra.c
+data(carbs)
+D <- carbs$D
+S <- carbs$S
+C <- carbs$C
 
-cc <- list(constraint("non-negativity"))
-cs <- list(constraint("non-negativity"))
+cc <- list(
+   constraint("non-negativity")
+)
 
-m <- mcrals(D, ncomp = 3, cont.constraints = cc, spec.constraints = cs)
-print(m)
+cs <- list(
+   constraint("non-negativity"),
+   constraint("norm", params = list(type = "area"))
+)
+
+set.seed(6)
+m <- mcrals(D, ncomp = 3, cont.constraints = cc, spec.constraints = cs, verbose = TRUE)
+summary(m)
+
+par(mfrow = c(2, 3))
+
+# resolved spectra vs new
+for (i in 1:min(c(m$ncomp, ncol(S)))) {
+   mdaplotg(
+      list(
+         original = prep.norm(mda.subset(mda.t(S), i), "area"),
+         resolved = prep.norm(mda.subset(mda.t(m$resspec), i), "area")
+      ), type = "l", col = c("black", "red"), lwd = c(3, 1), opacity = c(0.75, 1),
+      , xlim = c(1600, 200), xticks = seq(1600, 200, by = -200)
+   )
+}
+
+# resolved contributions vs new
+for (i in 1:min(c(m$ncomp, ncol(C)))) {
+   mdaplotg(
+      list(
+         original = prep.norm(mda.subset(mda.t(C), i), "area"),
+         resolved = prep.norm(mda.subset(mda.t(m$rescont), i), "area")
+      ), type = "l", col = c("black", "red"), lwd = c(3, 1), opacity = c(0.75, 1)
+   )
+}
+
+# check without constraints
+# check default seeints verbode = FALSE
+# check default seeints verbode = TRUE
