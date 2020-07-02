43a511618b51a5256f47bc65cc43dcea71cdbdc4
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2020-07-02 12:04:47 +0200

    improvements to tests for constraints, added tests for angle constraint

--- tests/testthat/test-constraints.R
+++ tests/testthat/test-constraints.R
@@ -1,58 +1,109 @@
-data(simdata)
-D <- simdata$spectra.c[1:5, ]
-D <- t(prep.snv(D))
-cn <- constraint("norm")
-Dr <- employ(cn, D)
-
+data(carbs)
+D <- carbs$D
+S <- carbs$S
+C <- carbs$C
 
 context("mcrals: test implemented constrains")
 
 for (cl in getImplementedConstraints()) {
    test_that(sprintf("%s works correctly with default parameters", cl$name), {
       expect_silent(cn <- constraint(cl$name, cl$params))
-      expect_silent(Dr <- employ(cn, D))
-      expect_true(is.matrix(D))
-      expect_equal(dim(Dr), dim(D))
+      expect_silent(Sr <- employ(cn, S, D))
+      expect_true(is.matrix(Sr))
+      expect_equal(dim(Sr), dim(S))
    })
 }
 
+#!############################
+#! Non-negativity constraint #
+#!############################
+
 # add one of the existing constraints with default parameters
-test_that("Non-negativity constraint", {
+test_that("Non-negativity constraint works correctly", {
+   S[sample(1:length(S))[1:10]] <- -10
    cn <- constraint("non-negativity")
-   Dr <- employ(cn, D)
-   expect_equal(sum(Dr < 0), 0)
+   Sr <- employ(cn, S, D)
+   expect_equal(sum(Sr < 0), 0)
 })
 
 test_that("Non-negativity constraint with user defined parameters (wrong)", {
    expect_error(cn <- constraint("non-negativity", params = list(type = "xxx")))
 })
 
+#!###########################
+#! Normalization constraint #
+#!###########################
+
 # add one of the existing constraints with default parameters
-test_that("Normalization constraint", {
+test_that("Normalization constraint works correctly", {
    cn <- constraint("norm")
-   Dr <- employ(cn, D)
-
-   # all columns should have unit area
-   expect_equivalent(colSums(abs(Dr)), rep(1, ncol(Dr)))
-})
-
-test_that("Non-negativity constraint with user defined parameters (correct)", {
-   cn <- constraint("norm", params = list(type = "length"))
-   Dr <- employ(cn, D)
+   Sr <- employ(cn, S, D)
 
    # all columns should have unit length
-   expect_equivalent(sqrt(colSums(Dr^2)), rep(1, ncol(Dr)))
+   expect_equivalent(colSums(Sr^2), rep(1, ncol(Sr)))
 })
 
-test_that("Non-negativity constraint with user defined parameters (wrong)", {
+test_that("Normalization constraint with user defined parameters (correct)", {
+   cn <- constraint("norm", params = list(type = "area"))
+   Sr <- employ(cn, S, D)
+
+   # all columns should have unit area
+   expect_equivalent(colSums(abs(Sr)), rep(1, ncol(Sr)))
+
+   cn <- constraint("norm", params = list(type = "sum"))
+   Sr <- employ(cn, S, D)
+
+   # all columns should have unit sum
+   expect_equivalent(colSums(Sr), rep(1, ncol(Sr)))
+})
+
+test_that("Normalization constraint with user defined parameters (wrong)", {
    expect_error(cn <- constraint("norm", params = list(method = "xxx")))
 })
 
-# add one of the existing constraints with non-default parameters
 
-# add one of the existing constraints with wrong parameters
-#cn <- constraint("non-negativity", thema = "dark")
-#Dr <- employ(cn, D)
+#!###################
+#! Angle constraint #
+#!###################
+
+# add one of the existing constraints with default parameters
+test_that("Angle constraint works correctly", {
+   cn <- constraint("angle")
+   Sr <- employ(cn, S, D)
+
+   # compute the expected values
+   m <- apply(D, 2, mean)
+   m <- m / sqrt(sum(m^2))
+   S <- t(prep.norm(t(S), "length"))
+   Se <- S * 0.95 + 0.05 * matrix(m, nrow(S), ncol(S))
+
+   # compare the expected and constrained values
+   expect_equivalent(Sr, Se)
+})
+
+test_that("Angle constraint with user defined parameters (correct)", {
+   cn <- constraint("angle", params = list(weight = 0))
+   Sr <- employ(cn, S, D)
+
+   # all columns should have unit length
+   expect_equivalent(Sr, t(prep.norm(t(S), "length")))
+
+   cn <- constraint("angle", params = list(weight = 1))
+   Sr <- employ(cn, S, D)
+
+   # compute the expected values
+   m <- apply(D, 2, mean)
+   m <- m / sqrt(sum(m^2))
+
+   expect_equivalent(Sr, matrix(m, nrow(S), ncol(S)))
+})
+
+test_that("Angle constraint with user defined parameters (wrong)", {
+   expect_error(cn <- constraint("norm", params = list(method = "xxx")))
+})
+
+
+# add one of the existing constraints with non-default parameters
 
 # check all available existing constraints with default parameters
 
